fe4958eeabe90902ebb6dd9e06182f6159ded862
mbougar <mbougar303@g.educaand.es>
2025-03-09 18:37:17 +0100

    Applied proper .gitignore

--- obj/Debug/net9.0/Test_API_PSP.AssemblyInfo.cs
+++ obj/Debug/net9.0/Test_API_PSP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test_API_PSP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2f8c0a805f37e949a5a8ff3961a61da6239317")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test_API_PSP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test_API_PSP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
